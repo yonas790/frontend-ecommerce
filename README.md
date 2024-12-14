588985249498b2ff7d3558dbf53313842889edb4
yonas790 <yoni2752@gmail.com>
2024-12-14 16:13:15 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### mern-ecommerce-frontend
+[Live URL](https://yon-naz-shop.netlify.app)
---
+++ src/utils/baseURL.js
@@ -0,0 +1,3 @@
+//  const baseURL = "http://localhost:2030/api/v1";
+const baseURL = "https://e-commerce-api-zc88.onrender.com/api/v1";
+export default baseURL;
